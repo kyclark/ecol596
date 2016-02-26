1ae054faf54c06154b23c9ce1cc3d0828702db32
Ken Youens-Clark <kyclark@gmail.com>
2016-02-26 09:16:21 -0700

    finding bounds now

--- project/make-plots.R
+++ project/make-plots.R
@@ -2,12 +2,16 @@ require(zoo)
 require(R.utils)
 
 argmax = function(x, y, w=1, ...) {
-  n        = length(y)
-  y.smooth = loess(y ~ x, ...)$fitted
-  y.max    = rollapply(zoo(y.smooth), 2*w+1, max, align="center")
-  delta    = y.max - y.smooth[-c(1:w, n+1-1:w)]
-  i.max    = which(delta <= 0) + w
-  list(x=x[i.max], i=i.max, y.hat=y.smooth)
+  n         = length(y)
+  y.smooth  = loess(y ~ x, ...)$fitted
+  y.max     = rollapply(zoo(y.smooth), 2*w+1, max, align="center")
+  y.min     = rollapply(zoo(y.smooth), 2*w+1, min, align="center")
+  delta.max = y.max - y.smooth[-c(1:w, n+1-1:w)]
+  delta.min = y.min - y.smooth[-c(1:w, n+1-1:w)]
+  i.max     = which(delta.max <= 0) + w
+  i.min     = which(delta.min >= 0) + w
+  
+  list(x=x[i.max], i=i.max, y.hat=y.smooth, y.min=i.min)
 }
 
 main = function() {
@@ -16,25 +20,44 @@ main = function() {
   w    = 1
   span = .4
   i    = 0
-
+  
   for (fname in list.files(dat.dir, pattern="*.fa$")) {
-    i = i + 1
-    printf("%5d: %s\n", i, fname)
+    
     dat = read.delim(file.path(dat.dir, fname), sep=" ", header=F)
     colnames(dat) = c("freq", "count")
     
-    x     = dat$freq
-    y     = dat$count
-    peaks = argmax(x=x, y=y, w=w, span=span)
+    x               = dat$freq
+    y               = dat$count
+    peaks           = argmax(x=x, y=y, w=w, span=span)
+    first.trough.y  = peaks$y.min[1]
+    first.trough.x  = x[first.trough.y][1]
+    first.peak.x    = x[peaks$i][1]
+    first.peak.y    = peaks$y.hat[peaks$i][1]
+    dist.to.mid     = first.peak.x - first.trough.x
+    second.trough.x = first.trough.x + dist.to.mid * 2
+    y.min           = min(y)
     
+    # write file to disk rather than display
     png(filename = paste0(fname, '.png'))
+    
+    # plot the original histogram
     plot(x, y, cex=0.75, col="Gray", main=fname, xlab="count", ylab="frequency")
+    
+    # draw the smoothed line
     lines(x, peaks$y.hat,  lwd=2) 
-    y.min = min(y)
-    sapply(peaks$i, function(i) lines(c(x[i],x[i]), c(y.min, peaks$y.hat[i]),
-                                      col="Red", lty=2))
-    points(x[peaks$i], peaks$y.hat[peaks$i], col="Red", pch=19, cex=1.25)
+    
+    # mark the first peak
+    points(x = first.peak.x, y = first.peak.y, col="Red", pch=19, cex=1.25)    
+    
+    # draw bars around the peak from left trough to same y on descending curve
+    segments(first.trough.x, y.min, first.trough.x, first.peak.y, col="Red") 
+    segments(second.trough.x, y.min, second.trough.x, first.peak.y, col="Red")
+    
+    # finish writing file
     dev.off()
+    i = i + 1
+    
+    printf("%5d: %s (%s - %s)\n", i, fname, first.trough.x, second.trough.x)
   }
   printf("Finished, processed %s file%s.\n", i, ifelse(i==1, '', 's'))
 }
